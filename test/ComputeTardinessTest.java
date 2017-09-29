ec535962aaf6e526f7f262d9f0200a4e49ee967e
opmaas <O.P.Maas@student.tudelft.nl>
2017-09-29 18:36:24 +0200

    Changed ComputeTardinessTest.java

--- test/ComputeTardinessTest.java
+++ test/ComputeTardinessTest.java
@@ -8,8 +8,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class ComputeTardinessTest {
     @Test
     void main() {
-        String[] arg = {"instances/random_RDD=0.2_TF=0.2_#10.dat"};
+        String[] arg = {"instances/random_RDD=0.2_TF=0.2_#5.dat"};
+        String[] arg2 = {"instances/random_RDD=0.2_TF=0.2_#10.dat"};
+        String[] arg3 = {"instances/random_RDD=0.2_TF=0.2_#15.dat"};
         ComputeTardiness.main(arg);
+        ComputeTardiness.main(arg2);
+        ComputeTardiness.main(arg3);
     }
 
 }
