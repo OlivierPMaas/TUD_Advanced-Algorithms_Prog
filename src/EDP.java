ac6a60297741189040f8737d8d20a16055e4df81
opmaas <O.P.Maas@student.tudelft.nl>
2017-10-03 13:14:28 +0200

    Fixed some flaws in subfunctions, everything should be working correctly now

--- src/EDP.java
+++ src/EDP.java
@@ -4,6 +4,7 @@
 public class EDP {
     private int numJobs;
     private int[][] jobs;
+    private Schedule greedyScheduleFixed;
 
     public EDP(ProblemInstance instance) {
         numJobs = instance.getNumJobs();
@@ -11,29 +12,61 @@ public class EDP {
         Greedy greedy = new Greedy(instance);
         //build up schedule, sorted in nondecreasing deadline order
         Schedule greedySchedule = greedy.getSchedule();
+        // Rewrite jobID's to be consistent with our algorithm's notation:
+        // We number the jobs in nondecreasing order.
+        // Note that we now lose the ability to return a schedule for the original jobs.
+        // That's fine for this assignment; we only need to return the optimal tardiness.
+        this.greedyScheduleFixed = resetIDs(greedySchedule, jobs);
+    }
+
+    public Schedule resetIDs(Schedule s, int[][] jobs) {
+        int depth = s.getDepth();
+        int jobDueTime = jobs[s.jobID][1];
+        // depth - 1 because we start counting at 0.
+        if(s.previous == null) {
+            return new Schedule(null, depth - 1, s.jobLength, jobDueTime);
+        }
+        return new Schedule(resetIDs(s.previous, jobs), depth - 1, s.jobLength, jobDueTime);
     }
 
     public int findOptimalTardiness() {
+        return computeOptimalTardiness(this.greedyScheduleFixed,0);
+    }
 
-        //return computeOptimalTardiness(s,0);
+    //CLEAN UP LATER
+    public void printer(Schedule s) {
+        if(s == null) {
+            return;
+        }
+        else {
+            System.out.println(s.jobID);
+            printer(s.previous);
+        }
+    }
+
+    public int computeOptimalTardiness(Schedule s, int startTime) {
+        // base cases!
+        
+
+
+
+        int j = s.getDepth()-1;
+        int t = startTime;
+        Schedule k = s.findK();
+        // Range over this soon
+        int delta = 0;//numJobs - s.getDepth();
+
+        Schedule S = s.removeK();
+        // -1, because we start counting at 0 instead of at 1 (as in Lawler 1977).
+        int kPrime = S.findK().jobID;
+        int value1 = computeOptimalTardiness(S.getScheduleBetween(0,kPrime + delta - 1).removeK(),t);
+        int value2 = 4;
+        int value3 = 4;
         return 4;
     }
+
 
     public int computeTardiness(Schedule s, int startTime) {
         return s.getTardiness() + startTime;
     }
-
-    public int computeOptimalTardiness(Schedule s, int startTime) {
-        int i = 0;
-        int j = s.getDepth()-1;
-        int t = startTime;
-        Schedule k = s.findK();
-        return 6;
-
-
-        //value1 = ;
-        //value2 = ;
-        //value3 = ;
-    }
-
 }
--- src/Schedule.java
+++ src/Schedule.java
@@ -4,9 +4,9 @@ public class Schedule implements Comparable<Schedule> {
 	// Feel free to modify it if you feel there exists a better one
 	// The main advantage is that in a search-tree there is a lot of overlap
 	// between schedules, this implementation stores this overlap only once
-	private Schedule previous;
-	private int jobID;
-	private int jobLength;
+	public Schedule previous;
+	public int jobID;
+	public int jobLength;
 	
 	// tardiness can be calculated instead of memorized
 	// however, we need to calculate it a lot, so we memorize it
@@ -42,17 +42,6 @@ public class Schedule implements Comparable<Schedule> {
 	}
 
 	// Used by EDP
-	// Gives the _n_'th job
-	public Schedule nthJob(int n) {
-		if(n == 0) {
-			return this;
-		}
-		else {
-			return this.previous.nthJob(n-1);
-		}
-	}
-
-	// Used by EDP
 	// Gives the job with highest processing time
 	public Schedule findK() {
 		// base case
@@ -68,18 +57,74 @@ public class Schedule implements Comparable<Schedule> {
 		}
 	}
 
+	// Used by EDP
+	public Schedule removeK() {
+		int k = this.findK().jobID;
+		return removeID(k);
+	}
+
+	// Used by EDP
+	public Schedule removeID(int id) {
+		if(this.jobID == id) {
+			return this.previous;
+		}
+		else {
+			Schedule output = this;
+			output.previous = this.previous.removeID(id);
+			return output;
+		}
+	}
+
 	public int getDepth(){
 		int depth = 1;
 		if(previous != null) depth += previous.getDepth();
 		return depth;
 	}
-	
+
+
+	// Used by EDP
+	public Schedule getScheduleBetween(int id, int id2) {
+		if(id < 0) {
+			throw new java.lang.Error("Tried to get too large a schedule 1");
+		}
+		// Put back id2 > this.jobID-case?
+		if(id2 <= this.jobID) {
+			return this.cutOffBefore(id);
+		}
+		else {
+			return this.previous.getScheduleBetween(id, id2);
+		}
+	}
+
+	public Schedule cutOffBefore(int id) {
+		if(this.jobID < id) {
+			return null;
+		}
+		else if(this.jobID == id) {
+			Schedule cutOff = this;
+			cutOff.previous = null;
+			return cutOff;
+		}
+		// if(this.jobID > id)
+		else {
+			System.out.println(this.jobID);
+			Schedule result = this;
+			if(this.previous != null) {
+				result.previous = this.previous.cutOffBefore(id);
+			}
+			else {
+				result.previous = null;
+			}
+			return result;
+		}
+	}
+
 	public int getTotalTime(){
 		int time = jobLength;
 		if(previous != null) time += previous.getTotalTime();
 		return time;
 	}
-	
+
 	public int getTardiness(){
 		return tardiness;
 	}
